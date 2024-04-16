f21e4e7c3877cab3b43799c29ee73b7e622289bd
AMAbdulrahim <98281994+AMAbdulrahim@users.noreply.github.com>
2024-04-16 18:52:37 +0300

    dark mode edit + files formating

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Your JavaScript code here
     var icon = document.getElementById("icon");
+    var logo = document.getElementById("logoimg");
     icon.onclick = function() {
         document.body.classList.toggle("dark-theme");
         if (document.body.classList.contains("dark-theme")) {
             icon.src = "/images/sun.png";
+            logo.src = "/images/logo_white.png"
         } else {
             icon.src = "/images/moon.png";
+            logo.src = "/images/logo.png"
+            
         }
     };
 });
