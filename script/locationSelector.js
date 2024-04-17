5ede7530a24bab618fa6663caefc4370f5ce2fc6
202042820 <s202042820@kfup.edu.sa>
2024-04-17 23:15:24 +0300

    ece/event css+media

---
+++ script/locationSelector.js
@@ -0,0 +1,14 @@
+
+    const getlocation = () =>{
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
+
+    function showPosition(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        alert("Latitude: " + latitude + ", Longitude: " + longitude);
+    }
