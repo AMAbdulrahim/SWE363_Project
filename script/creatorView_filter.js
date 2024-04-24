73a5bd9d330298d829122baf784e792b1e0c8478
AMAbdulrahim <98281994+AMAbdulrahim@users.noreply.github.com>
2024-04-24 16:29:51 +0300

    deployment files organaziation

--- script/creatorView_filter.js
+++ script/creatorView_filter.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const selectFilter = document.getElementById("select_filter");
     const filterButton = document.getElementById("search_button_creator");
 
-    // Function to filter events based on search input and selected option
     function filterEvents() {
         const searchText = searchInput.value.toLowerCase();
         const selectedOption = selectFilter.value;
@@ -15,13 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (selectedOption === "prevSelect") {
             filterEventCards(previousEvents, searchText);
         } else {
-            // Reset the search if no valid option is selected
             currentEvents.forEach(card => card.style.display = "");
             previousEvents.forEach(card => card.style.display = "");
         }
     }
 
-    // Function to filter event cards based on search text
     function filterEventCards(events, searchText) {
         events.forEach(card => {
             const eventName = card.querySelector(".title-card h1").textContent.toLowerCase();
@@ -33,15 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Event listener for search input
     searchInput.addEventListener("input", filterEvents);
 
-    // Event listener for select filter
     selectFilter.addEventListener("change", filterEvents);
 
-    // Event listener for button click
     filterButton.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the default form submission behavior
-        filterEvents(); // Call the filter function when the button is clicked
+        event.preventDefault(); 
+        filterEvents(); 
     });
 });
