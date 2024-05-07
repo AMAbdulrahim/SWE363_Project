a8113e6767c72fe44387d78cbe5ce1f730488897
AMAbdulrahim <98281994+AMAbdulrahim@users.noreply.github.com>
2024-05-07 21:50:56 +0300

    store darkMode prefrence when changing the page

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Your JavaScript code here
     var icon = document.getElementById("icon");
     var logo = document.getElementById("logoimg");
-    icon.onclick = function() {
+
+    // Function to toggle dark mode
+    function toggleDarkMode() {
         document.body.classList.toggle("dark-theme");
         if (document.body.classList.contains("dark-theme")) {
             icon.src = "/images/sun.png";
-            logo.src = "/images/logo_white.png"
+            logo.src = "/images/logo_white.png";
         } else {
             icon.src = "/images/moon.png";
-            logo.src = "/images/logo.png"
-            
+            logo.src = "/images/logo.png";
         }
-    };
+        // Save the current mode preference to localStorage
+        localStorage.setItem('darkMode', document.body.classList.contains('dark-theme'));
+    }
+
+    // Event listener for dark mode toggle
+    icon.onclick = toggleDarkMode;
+
+    // Check if dark mode preference is saved in localStorage and apply it
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    if (isDarkMode) {
+        document.body.classList.add('dark-theme');
+        icon.src = "/images/sun.png";
+        logo.src = "/images/logo_white.png";
+    }
 });
